d34a215abca4c4a9ca9af3fdce6c0bde1c8a52df
mandeep <singh.mr.1996@gmail.com>
2021-06-04 21:32:31 +0530

    uc2_compute_one

--- sortingAndArithmeticComputationProblem.sh
+++ sortingAndArithmeticComputationProblem.sh
@@ -3,3 +3,5 @@ echo "Arithmetic Computation & Sorting"
 read -p "Enter first input :" firstInput
 read -p "Enter second input:" secondInput
 read -p "Enter third input :" thirdInput
+result=$((firstInput+$secondInput*$thirdInput))
+echo "Result:" $result
